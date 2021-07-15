bfde122f40883f8d372a277bfa3d8d1448deb694
oleksandr burkotenko <o.burkotenko@lemonmind.com>
2021-07-15 15:50:56 +0200

    return json with objects data from IndexService

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\Annotation\Route;
+use Pimcore\Bundle\EcommerceFrameworkBundle\Factory;
+use \Pimcore\Model\DataObject\ProductCategory;
 
 class DefaultController extends FrontendController
 {
@@ -17,5 +19,29 @@ class DefaultController extends FrontendController
     public function defaultAction(Request $request)
     {
         return [];
+    }
+
+    /**
+     * @Route("/service")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function academyAction() {
+
+        $products = [];
+
+        $productList = Factory::getInstance()->getIndexService()->getProductListForTenant('default');
+        //$productList->setCategory(ProductCategory::getById(5));
+
+        foreach($productList as $product) {
+            $products[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'price' => $product->getPrice(),
+                'text' => $product->getText(),
+            ];
+        }
+
+        return $this->json($products);
+
     }
 }
