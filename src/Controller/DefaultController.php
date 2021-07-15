fa5f826f8ca095fc25c2bdad83591649fd02b0b9
oleksandr burkotenko <o.burkotenko@lemonmind.com>
2021-07-15 16:14:24 +0200

    add items to cart and list car items action

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\Annotation\Route;
 use Pimcore\Bundle\EcommerceFrameworkBundle\Factory;
+use Pimcore\Model\DataObject\Product;
 use \Pimcore\Model\DataObject\ProductCategory;
 
 class DefaultController extends FrontendController
@@ -40,8 +41,45 @@ class DefaultController extends FrontendController
                 'text' => $product->getText(),
             ];
         }
-
         return $this->json($products);
+    }
 
+    /**
+     * @Route("/service/cartadd", name="cartadd")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function addToCartAction(Request $request)
+    {
+        $product = Product::getById($request->get("id"));
+        $cartManager = Factory::getInstance()->getCartManager();
+        $cart = $cartManager->getOrCreateCartByName('my-cart');
+
+        $cart->addItem($product, 3);
+        $cart->save();
+
+        return $this->redirectToRoute("cartlist");
+    }
+
+     /**
+     * @Route("/service/cartlist", name="cartlist")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function cartListAction()
+    {
+        $cartItems = [];
+        $cartManager = Factory::getInstance()->getCartManager();
+        $cart = $cartManager->getOrCreateCartByName('my-cart');
+
+        foreach ($cart->getItems() as $item) {
+            $cartItems[] = [
+                'id' => $item->getItemKey(),
+                'name' => $item->getProduct()->getName(),
+                'count' => $item->getCount(),
+                'price' => $item->getProduct()->getPrice(),
+            ];
+        }
+
+        return $this->json($cartItems);
     }
 }
